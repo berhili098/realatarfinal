e521cd713e372d77592ce6454b3d2fa8953113f4
abdessamad <a.berahhou@edu.umi.ac.ma>
2022-08-15 11:46:55 +0100

    assets

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Client extends Model
 {
-    use HasFactory;
-    protected $fillable = ['uid','fullname','email','password','birthday','city','phone','gender','avatar','level','points','isPremium','isVerified','status','delete'];
+    use HasFactory, HasApiTokens;
+    protected $fillable = ['uid', 'fullname', 'email', 'password', 'birthday', 'city', 'phone', 'gender', 'avatar', 'level', 'points', 'isPremium', 'isVerified', 'status', 'delete'];
     protected $table = 'clients';
     public $primaryKey = 'uid';
     protected $keyType = "string";
     public $incrementing = false;
-    // public function saveds(){
-    //     return $this->hasMany(saved::class, 'client_id');
-
-//}
-
+    public function saveds()
+    {
+        return $this->hasMany(saved::class, 'client_id');
+    }
 }
--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,7 +15,8 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->morphs('tokenable');
+            $table->string('tokenable_type')->index();
+            $table->string('tokenable_id')->index();
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
