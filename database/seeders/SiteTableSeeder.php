eb56ab27608a23a319c589d4e8f39f09fdf199df
VTapp1 <covidtrackerapp97@gmail.com>
2022-08-22 12:15:59 +0100

    f

--- database/seeders/SiteTableSeeder.php
+++ database/seeders/SiteTableSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\City;
+use App\Models\Site;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
-class CitiesTableSeeder extends Seeder
+use Faker\Factory as Faker;
+class SiteTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -20,19 +20,21 @@ class CitiesTableSeeder extends Seeder
         $faker_en = Faker::create('en_EN');
         foreach(range(1,20) as $index)
         {
-            City::create([
-                'city_ar' => $faker_ar->name,
-                'city_fr' => $faker_fr->name,
-                'city_en' => $faker_en->name,
+            Site::create([
+                'name_ar' => $faker_ar->name,
+                'name_fr' => $faker_fr->name,
+                'name_en' => $faker_en->name,
                 'description_ar' => $faker_ar->text(500),
                 'description_fr' => $faker_fr->paragraph(3),
                 'description_en' => $faker_en->paragraph(3),
                 'user_id' => 1,
+                'openTime' => $faker_fr->time('H:i:s'),
+                'duration'=> $faker_fr->numberBetween(1,10),
+                'price'=> $faker_fr->numberBetween(1,10),
                 'longitude'=> $faker_fr->longitude,
                 'latitude'=> $faker_fr->latitude,
-                
+                'city_id'=> $faker_fr->numberBetween(1,10),
             ]);
         }
-        
     }
 }
